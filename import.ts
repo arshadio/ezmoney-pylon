f04f2a952bcf28b4daffb36107dcb7906fbc5f00
h arshad <viirall@users.noreply.github.com>
2020-11-21 13:54:59 -0500

    Create import.ts, list imports.

---
+++ import.ts
@@ -0,0 +1,22 @@
+/* if you plan on copy pasting and using this code from here,
+just copy and paste these imports into 'main.ts',
+which should allow the code to run.
+*/
+
+import './ezmoney/backup/test';
+
+import './ezmoney/commands/admin';
+import './ezmoney/commands/master';
+import './ezmoney/commands/profile';
+
+import './ezmoney/config/levelling';
+
+import './ezmoney/currency/gamble';
+import './ezmoney/currency/timed-tasks';
+
+import './ezmoney/inventory/inv-setup';
+import './ezmoney/inventory/inventory';
+import './ezmoney/inventory/shop';
+
+import './ezmoney/steal/steal-func';
+import './ezmoney/steal/steal';
