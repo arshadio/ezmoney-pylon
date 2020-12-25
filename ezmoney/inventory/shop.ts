1c45a09d77d258f74a93107a8b5b5cf045cfe301
h arshad <viirall@users.noreply.github.com>
2020-12-24 21:08:47 -0500

    make function more dynamic

--- ezmoney/inventory/shop.ts
+++ ezmoney/inventory/shop.ts
@@ -4,6 +4,57 @@ import * as inventory from './inv-setup';
 import { Item } from './inv-setup';
 import obc from '../config/setup';
 
+// new unknownItem message
+const unknownItem = (name: string) => {
+  return `Item with name: **${name}** does not exist, use \`.shop\` to list items that can be purchased.`;
+};
+
+// new
+const getObjectProperty = (name: string, obj: object) => {
+  const arr = obj;
+  for (const property in arr) {
+    if (property.toLowerCase().includes(name))
+      // @ts-ignore
+      return arr[property];
+  }
+};
+
+// new
+export const buildShopEmbed = (name: string): discord.Embed => {
+  name = name.toLowerCase();
+  for (const [item, itemInfo] of inventory.itemConfig.entries()) {
+    if (
+      itemInfo.id.toLowerCase() === name ||
+      itemInfo.shortform.toLowerCase() === name ||
+      name === Item.gemID
+    ) {
+      return new discord.Embed({
+        title: itemInfo.name,
+        description: `**Type: \`${
+          itemInfo.itemType
+        }\`** \n\n${getObjectProperty(
+          name,
+          inventory.extendDescriptions
+        )} \n\n${[
+          `**PRICE** - **${
+            name === Item.gemID
+              ? 'Cannot be Purchased'
+              : `${def.standards.currency}${itemInfo.price}`
+          }**`,
+          `**SELLS** - **${def.standards.currency}${
+            name === Item.gemID
+              ? `${inventory.ItemConfig.gemPrice}`
+              : `${Math.round(itemInfo.price / 10)}`
+          }**`
+        ].join('\n')}`,
+        color: def.standards.embeds.general,
+        thumbnail: { url: getObjectProperty(name, inventory.itemThumbnails) }
+      });
+    }
+  }
+  return new discord.Embed({ description: unknownItem(name) });
+};
+
 obc.subcommand('shop', async (shopCommands) => {
   shopCommands.default(
     (a) => ({ shopItem: a.stringOptional() }),
@@ -26,82 +77,12 @@ obc.subcommand('shop', async (shopCommands) => {
         const item = inventory.findItemByName(shopItem);
         if (item == null) {
           return message.reply({
-            content: `Item with name: **${shopItem}** does not exist, use \`.shop\` to list items that can be purchased.`,
+            content: unknownItem(shopItem),
             allowedMentions: {}
           });
         }
         const auditItem = shopItem.toLowerCase();
-        switch (auditItem) {
-          case Item.padlockID:
-          case Item.padlockSF:
-            const userLocks = await inventory.getLocks(message.author.id);
-            const lockEmbed = inventory.newEmbed(
-              `**${Item.padlockName}** ${
-                userLocks >= 1 ? `(${userLocks} Owned)` : ''
-              }`,
-              Item.padlockPrice,
-              inventory.ItemTypes.Tool,
-              inventory.extendDescriptions.padlock,
-              'https://images.emojiterra.com/twitter/512px/1f512.png'
-            );
-            await message.reply(lockEmbed);
-            break;
-          case Item.landmineID:
-          case Item.landmineSF:
-            const userMines = await inventory.getMines(message.author.id);
-            const lmEmbed = inventory.newEmbed(
-              `**${Item.landmineName}** ${
-                userMines >= 1 ? `(${userMines} Owned)` : ''
-              }`,
-              Item.landminePrice,
-              inventory.ItemTypes.Tool,
-              inventory.extendDescriptions.landmine,
-              'https://images.emojiterra.com/twitter/v13.0/512px/1f9e8.png'
-            );
-            await message.reply(lmEmbed);
-            break;
-          case Item.lifesaverID:
-          case Item.lifesaverSF:
-            const userLives = await inventory.getLives(message.author.id);
-            const lifeEmbed = inventory.newEmbed(
-              `**${Item.lifesaverName}** ${
-                userLives >= 1 ? `(${userLives} Owned)` : ''
-              }`,
-              Item.lifesaverPrice,
-              inventory.ItemTypes.PowerUp,
-              inventory.extendDescriptions.heart,
-              'https://creazilla-store.fra1.digitaloceanspaces.com/emojis/54393/red-heart-emoji-clipart-md.png'
-            );
-            await message.reply(lifeEmbed);
-            break;
-          case Item.pickaxeID:
-          case Item.pickaxeSF:
-            const userPicks = await inventory.getPicks(message.author.id);
-            const pickEmbed = inventory.newEmbed(
-              `**${Item.pickaxeName}** ${
-                userPicks >= 1 ? `(${userPicks} Owned)` : ''
-              }`,
-              Item.pickaxePrice,
-              inventory.ItemTypes.Tool,
-              inventory.extendDescriptions.pickaxe,
-              'https://images.emojiterra.com/twitter/v13.0/512px/26cf.png'
-            );
-            await message.reply(pickEmbed);
-            break;
-          case Item.gemID:
-            const userGems = await inventory.getGems(message.author.id);
-            const gembed = inventory.newEmbed(
-              `**${Item.gemName}** ${
-                userGems >= 1 ? `(${userGems} Owned)` : ''
-              }`,
-              0,
-              inventory.ItemTypes.Collectible,
-              inventory.extendDescriptions.gem,
-              'https://images.emojiterra.com/twitter/v13.0/512px/1f48e.png'
-            );
-            await message.reply(gembed);
-            break;
-        }
+        await message.reply(buildShopEmbed(auditItem));
       }
     }
   );
